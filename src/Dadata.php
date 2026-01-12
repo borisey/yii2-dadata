a052eceeb93a09001e6616d39d8f0891cd98c5c8
borisey <51847223+borisey@users.noreply.github.com>
2026-01-12 19:47:29 +0300

    fix

--- src/Dadata.php
+++ src/Dadata.php
@@ -6,11 +6,39 @@ use yii\base\Component;
 
 class Dadata extends Component
 {
+    const CLEANER_URL    = 'https://cleaner.dadata.ru/api/v1/clean/address';
+    const SUGGESTION_URL = 'https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest';
+
     public string $token;
     public string $secret;
 
     public function getFirm()
     {
-        return $this->token . ' ' . $this->secret;
+        $fields = [
+            'query' => 'ooo ромашка'
+        ];
+
+        $url = self::SUGGESTION_URL . '/party';
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+            "Content-Type: application/json",
+            "Accept: application/json",
+            "Authorization: Token " . $this->token
+        ));
+        curl_setopt($curl, CURLOPT_POST, 1);
+
+
+        if ($fields != null) {
+            curl_setopt($curl, CURLOPT_POST, 1);
+            curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($fields));
+        } else {
+            curl_setopt($curl, CURLOPT_POST, 0);
+        }
+        $result = $this->exec();
+        $result = json_decode($result, true);
+
+        return $result;
     }
 }
