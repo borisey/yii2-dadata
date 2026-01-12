b05119387b4401bcb03947a06ed9826092887d3d
borisey <51847223+borisey@users.noreply.github.com>
2026-01-12 19:24:39 +0300

    fix

--- src/Dadata.php
+++ src/Dadata.php
@@ -6,8 +6,11 @@ use yii\base\Component;
 
 class Dadata extends Component
 {
+    public string $token;
+    public string $secret;
+
     public function getFirm()
     {
-        return 123;
+        return $this->token . ' ' . $this->secret;
     }
 }
